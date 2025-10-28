c5df8e0374ea5c323601b1b180e1612bfd2a5056
Tobi Idowu <tobididowu@icloud.com>
2025-10-28 16:38:04 +0000

    defined the exception classes

---
+++ src/com/mthree/tobiidowu/flooringMastery/exception/NoSuchOrderException.java
@@ -0,0 +1,12 @@
+package com.mthree.tobiidowu.flooringMastery.exception;
+
+public class NoSuchOrderException extends Exception {
+    // constructors
+    public NoSuchOrderException() {
+        super();
+    }
+
+    public NoSuchOrderException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/mthree/tobiidowu/flooringMastery/exception/PersistenceException.java
@@ -0,0 +1,12 @@
+package com.mthree.tobiidowu.flooringMastery.exception;
+
+public class PersistenceException extends Exception {
+    // constructors
+    public PersistenceException() {
+        super();
+    }
+
+    public PersistenceException(String message) {
+        super(message);
+    }
+}
